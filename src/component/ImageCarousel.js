e19c9d3bce0219b7d87841d92a6596bc63df13c3
manojgunjal023 <manojgunjal023@gmail.com>
2024-08-02 11:13:47 +0530

    adding carasoul images

---
+++ src/component/ImageCarousel.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './ImageCarousel.css';
+
+const images = [
+  './images/Senior1.jpg',
+  './images/Senior2.jfif',
+  './images/Senior3.jpg',
+  './images/Senior4.png',
+];
+
+class ImageCarousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentIndex: 0 };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.nextImage, 3000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  nextImage = () => {
+    this.setState(prevState => ({
+      currentIndex: (prevState.currentIndex + 1) % images.length,
+    }));
+  };
+
+  render() {
+    return (
+      <div className="carousel">
+        <img src={images[this.state.currentIndex]} alt="carousel" className="carousel-image" />
+      </div>
+    );
+  }
+}
+
+export default ImageCarousel;
