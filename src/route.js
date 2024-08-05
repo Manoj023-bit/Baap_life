229debabedd0eb18da0fa6e3cae9faf7e682a1df
manojgunjal023 <manojgunjal023@gmail.com>
2024-08-05 10:41:45 +0530

    changes of nabar ui fixation

--- src/route.js
+++ src/route.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./component/Navbar";
 import Admission from "./component/pages/Admisson/Admission";
 import OurStudents from "./component/pages/Our students/OurStudents";
+import CustomNavbar from "./component/Navbar/Navbar";
 function RouteSetting() {
   return (
     <Routes>
-      <Route path="/" element={<Navbar />} />
+      <Route path="/" element={< CustomNavbar/>} />
       <Route path="/Admission" element={<Admission />} />
       <Route path="/our students" element={<OurStudents />} />
 
