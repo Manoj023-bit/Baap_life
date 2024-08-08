9e0ca029b5f63394adc3bdafb8439ed087775ba6
saish-nehe <saishnehe48.com>
2024-08-08 16:52:13 +0530

    changes of code

--- src/route.js
+++ src/route.js
@@ -5,6 +5,7 @@ import OurStudents from "./component/pages/Our students/OurStudents";
 // import CustomNavbar from "./component/Navbar/Navbar";
 import Home from "./component/pages/Home/Home"
 import About from "./component/pages/About/About"
+import Health from "./component/pages/Health.js/Health";
 function RouteSetting() {
   return (
     <Routes>
@@ -13,7 +14,7 @@ function RouteSetting() {
       <Route path="/Admission" element={<Admission />} />
       <Route path="/ourstudents" element={<OurStudents />} />
       <Route path="/about" element={<About/>} />
-
+      <Route path="/health" element={<Health/>} />
     </Routes>
   );
 }
